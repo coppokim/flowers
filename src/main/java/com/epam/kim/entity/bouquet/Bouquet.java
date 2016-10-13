c0c2923b23b173f66ae0eb87857285eb024d8b6f
coppokim <vasiliy0303@mail.ru>
2016-10-14 01:11:50 +0600

    added FlowerFactory

--- src/main/java/com/epam/kim/entity/bouquet/Bouquet.java
+++ src/main/java/com/epam/kim/entity/bouquet/Bouquet.java
@@ -3,16 +3,31 @@ package com.epam.kim.entity.bouquet;
 import com.epam.kim.entity.flowers.Flower;
 import com.epam.kim.entity.wrapper.Wrapper;
 
-public class Bouquet {
-    public Flower flower [];
-    public Wrapper wrapper;
+import java.util.ArrayList;
+import java.util.List;
 
-    public int bouqetPrice(){
-        int k=0;
-        for (int i=0;i<flower.length;i++){
-            k+=flower[i].getPrice();
-        }
-        k+=wrapper.getPrice();
-        return k;
+public class Bouquet {
+    private List<Flower> flowerList = new ArrayList<Flower>();
+    private Wrapper wrapper;
+
+    public void addFlower(Flower flower, int flowerCount){
+        for (int i=0;i<flowerCount;i++)
+        this.flowerList.add(flower);
     }
+    public List<Flower> getFlowerList() {
+        return flowerList;
+    }
+
+    public void setFlowerList(List<Flower> flowerList) {
+        this.flowerList = flowerList;
+    }
+
+    public Wrapper getWrapper() {
+        return wrapper;
+    }
+
+    public void setWrapper(Wrapper wrapper) {
+        this.wrapper = wrapper;
+    }
+
 }
